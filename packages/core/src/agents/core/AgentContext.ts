f6ce32d6cd3c235041905077a4c79bd13d8b6087
bundy <1695365384@qq.com>
2026-04-02 12:47:05 +0800

    fix: 移除 AgentContext 中不存在的 setEnvironmentContext 调用
    AgentContext 自身已存储 environmentContext，runner 通过
    context.environmentContext 访问，无需额外设置。

--- packages/core/src/agents/core/AgentContext.ts
+++ packages/core/src/agents/core/AgentContext.ts
@@ -53,9 +53,6 @@ export class AgentContextImpl implements AgentContext {
 
     this.providerManager = new ProviderManager({ externalConfig });
     this.runner = new AgentRunner(this.providerManager);
-    if (environmentContext) {
-      this.runner.setEnvironmentContext(environmentContext);
-    }
     this.skillRegistry = createSkillRegistry(skillConfig);
     this.agentRegistry = new AgentRegistryImpl();
     this.hookRegistry = new HookRegistry();
